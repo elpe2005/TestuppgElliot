a4e94c7758986b49f15aa9f64f87833baef69685
Admin <Admin@TEB12707>
2023-02-17 10:32:50 +0100

    Add project files.

---
+++ TestuppgElliot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv in två heltal");
+            Console.WriteLine($"{AREA(int.Parse(Console.ReadLine()),int.Parse(Console.ReadLine()))}");
+        }
+        static int AREA(int tal1,int tal2)
+        {
+            int A = tal1 * tal2;
+            return A;
+        }
+    }
+}
